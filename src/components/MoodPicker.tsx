bf3cac43782c927d63c39931e4ea07e05a176020
Daniel Atonge <elamboatonge@gmail.com>
2022-07-23 14:33:38 +0300

    Adds Icons

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, Text, StyleSheet, Pressable, Image } from 'react-native';
 import React, { useCallback, useState } from 'react';
 import { MoodOptionType } from '../types';
 import { theme } from '../theme';
@@ -15,18 +15,40 @@ type MoodPickerProps = {
   onSelectMood: (moodOption: MoodOptionType) => void;
 };
 
+const imageSrc = require('../../assets/butterflies.png');
+
 export const MoodPicker: React.FC<MoodPickerProps> = ({ onSelectMood }) => {
   const [selectedMood, setSelectedMood] = useState<MoodOptionType>();
+  const [hasSelected, setHasSelected] = useState(false);
 
   const moodSelected = useCallback(() => {
     if (selectedMood) {
       onSelectMood(selectedMood);
       setSelectedMood(undefined);
+      setHasSelected(true);
     }
   }, [onSelectMood, selectedMood]);
+
+  if (hasSelected) {
+    return (
+      <View style={styles.moodPickerWrapper}>
+        <Image
+          source={imageSrc}
+          style={{ alignSelf: 'center', marginBottom: 20 }}
+        />
+        <Pressable onPress={() => setHasSelected(false)} style={styles.button}>
+          <Text style={{ fontWeight: 'bold', color: 'white' }}>
+            Choose another!
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.moodPickerWrapper}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'black' }}>
+      <Text
+        style={{ fontSize: 20, fontWeight: 'bold', color: theme.colorWhite }}>
         How are you right now?
       </Text>
       <View style={styles.iconsContainer}>
@@ -96,6 +118,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 26,
     borderWidth: 2,
+    borderColor: theme.colorPurple,
     borderRadius: 10,
+    backgroundColor: 'rgba(0,0,0,0.2)',
   },
 });
--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -1,17 +1,17 @@
-import { View, StyleSheet, Image } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import React from 'react';
 import { MoodPicker } from '../components/MoodPicker';
 import { useAppContext } from '../App.provider';
 
-const imageSrc = require('../../assets/butterflies.png');
+const imageUrl =
+  'https://images.unsplash.com/photo-1474540412665-1cdae210ae6b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1766&q=80';
 
 export const Home = () => {
   const appContext = useAppContext();
   return (
-    <View style={styles.container}>
-      <Image source={imageSrc} />
+    <ImageBackground source={{ uri: imageUrl }} style={styles.container}>
       <MoodPicker onSelectMood={appContext.onSelected} />
-    </View>
+    </ImageBackground>
   );
 };
 
