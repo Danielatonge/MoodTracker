b68043d990a47f3ed5c7f49c6d3060c126155bce
Daniel Atonge <elamboatonge@gmail.com>
2022-07-24 21:08:56 +0300

    Data Visualization

--- src/screens/Analytics.screen.tsx
+++ src/screens/Analytics.screen.tsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native';
+import { groupBy } from 'lodash';
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { VictoryPie } from 'victory-native';
+import { useAppContext } from '../App.provider';
 
 export const Analytics = () => {
+  const appContext = useAppContext();
+
+  const data = Object.entries(groupBy(appContext.moodList, 'mood.emoji')).map(
+    ([key, value]) => ({
+      x: key,
+      y: value.length,
+    }),
+  );
+
   return (
-    <View>
-      <Text>Analytics</Text>
+    <View style={styles.container}>
+      <VictoryPie data={data} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+});
