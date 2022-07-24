6f641f9c760182981184c5e63498b750ce5ae905
Daniel Atonge <elamboatonge@gmail.com>
2022-07-24 20:43:52 +0300

    Android splash screen

--- android/app/src/main/java/com/moodtracker/MainActivity.java
+++ android/app/src/main/java/com/moodtracker/MainActivity.java
@@ -5,6 +5,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -29,7 +31,8 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
 
   /**
